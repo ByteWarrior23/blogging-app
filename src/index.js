ca5495c2d4d3ad9e361abb30944c2763f6188bb6
Harsh Agrawal <harshagrawal180806@gmail.com>
2025-08-20 13:54:31 +0530

    Utils code update

--- src/index.js
+++ src/index.js
@@ -7,14 +7,13 @@ dotenv.config({
     path: "./.env",
 });
 
-connectDB();
+connectDB()
+.then(() =>
+app.listen(process.env.PORT || 8000 , () =>{
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+}) )
+.catch((error) => {
+    console.error("Failed to connect to the database:", error)
+    process.exit(1)
+});
 
-/*( async () => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    }
-    catch(error){
-        console.log(error); 
-        throw error;
-    }
-})()*/
