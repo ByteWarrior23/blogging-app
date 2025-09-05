8843b8f794c20eda30b84bef24eac4e5fd14a3f8
Harsh Agrawal <harshagrawal180806@gmail.com>
2025-09-05 20:10:26 +0530

    worked on user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,64 @@
 import asyncHandler from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
+import {ApiResponse} from "../utils/ApiResponse.js"   
 
 const registerUser = asyncHandler(async (req, res) => {
-    return res.status(200).json({
-        message: "ok"
-    });
+    const { userName, fullName, email, password } = req.body;
+    console.log("Email:", email);
+    if (
+        [fullName, email, userName, password].some(
+            (field) => typeof field !== "string" || field.trim() === ""
+        )
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+    const existedUser = User.findOne({
+        $or : [{username},{email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User with same email or username Already Exits")
+    }
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar is required")
+    }
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudianary(avatarLocalPath)
+    const coverImage = await uploadOnCloudianary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(500, "Failed to upload avatar")
+    }
+    if(!coverImage){
+        throw new ApiError(500, "Failed to upload cover image")
+    }
+
+    const user = await User.create({
+        username : userName.toLowerCase(),
+        fullName,
+        email,
+        password,
+        avatar : avatar.secure_url,
+        coverImage : coverImage.secure_url
+    })
+
+    const createdUser = User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    //already selected so remove by -password -refreshtoken
+    
+    if(!createdUser){
+        throw new ApiError(500,"Something Went Wrong while registration")
+    }
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+        )
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,94 +1,97 @@
-import mongoose , {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const UserSchema = new Schema(
-    {
-        username :{
-            type : String ,
-            required : true ,
-            unique : true,
-            lowecase : true,
-            trim : true,
-            index : true
-        },
-        email :{
-            type : String ,
-            required : true ,
-            unique : true,
-            lowecase : true,
-            trim : true,
-        },
-        fullname :{
-            type : String ,
-            required : true ,
-            trim : true,
-            index : true
-        },
-        avatar :{
-            type : String ,
-            required : true,
-        },
-        coverImage :{
-            type : String ,
-        },
-        watchHistory :[{
-            type : Schema.Types.ObjectId,
-            ref : "video.model"
-        }
-    ],
-        password :{
-            type : String ,
-            required : [true,"Password is required"], 
-            trim : true,
-            minlength : 8                                
-        },
-        refreshToken :{
-            type : String ,
-    }
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true, // fixed typo
+      trim: true,
+      index: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true, // fixed typo
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video.model",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      trim: true,
+      minlength: 8,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generate Access Token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
     {
-        timestamps : true
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-)  
+  );
+};
 
-UserSchema.pre("save",async function(next){
-    if(!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password,10);
-    next();
-}
-)
+// Generate Refresh Token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.password);
-}
-
-UserSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            //this is payload data
-            _id : this._id,
-            username : this.username,
-            email : this.email,
-            fullname : this .fullname
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
-        }
-    );
-}
-
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id : this._id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
-        }
-    );
-}
-
-export const User = mongoose.model("User",UserSchema)
+export const User = mongoose.model("User", userSchema);
